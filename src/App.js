95a6d0c6768ac564403e7bb949ed0a913d7bf7ea
Tim <firus227@gmail.com>
2025-01-18 17:43:45 +0300

    add frog

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { useWallet } from "@solana/wallet-adapter-react";
-
+import fuckImg from "./images/fuck.png"
 function App() {
   const { publicKey } = useWallet();
 
   return (
     <div className="App">
       <header className="App-header">
-      <div style={{ padding: "20px", textAlign: "center" }}>
-            <h1>Fuck the Trump</h1>
-            <WalletMultiButton />
+        <div style={{ padding: "20px", textAlign: "center" }}>
+          <img src={fuckImg} alt="" style={{ width: "50%", maxWidth: "600px"}}/>
+          <h1 style={{ marginTop: -50}} >Fuck the Trump</h1>
+          <WalletMultiButton />
+
         </div>
 
       </header>
